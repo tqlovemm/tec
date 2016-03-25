08450f2a4b02349ed671c33a09750840fc411968
tqmm <397114202@qq.com>
2016-03-25 10:06:23 +0800

    fewf

--- footer.php
+++ footer.php
@@ -127,6 +127,6 @@
 </script>
 <div class="bdsharebuttonbox hidden"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_qzone" data-cmd="qzone"></a><a href="#" class="bds_tsina" data-cmd="tsina"></a><a href="#" class="bds_tqq" data-cmd="tqq"></a><a href="#" class="bds_renren" data-cmd="renren"></a><a href="#" class="bds_weixin" data-cmd="weixin"></a></div>
 <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdPic":"","bdStyle":"0","bdSize":"16"},"share":{},"image":{"viewList":["qzone","tsina","tqq","renren","weixin"],"viewText":"分享到：","viewSize":"16"},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
-
+<span style="display: none;"><script src="http://s11.cnzz.com/z_stat.php?id=1258178253&web_id=1258178253" language="JavaScript"></script></span>
 </body>
 </html>
